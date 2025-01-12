cf1a63859fda809b95deb97e0fc85ddd2fbe542c
matias valdivieso <e1725433203@uisrael.edu.ec>
2025-01-12 08:51:05 -0500

    Agregar todo el proyecto de la semana 5 a github

---
+++ src/test/java/com/tarea5/Config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea5.Config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
